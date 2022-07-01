3104bde381b844e4f199678a9ceaab2badeced34
Diego Rodrigues <adv.diego21@gmail.com>
2022-07-01 18:49:34 -0300

    adição de novos arquivos

--- database/migrations/2022_07_01_103337_create_fornecedores_table.php
+++ database/migrations/2022_07_01_103337_create_fornecedores_table.php
@@ -13,14 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('site_contatos', function (Blueprint $table) {
+        Schema::create('fornecedores', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
             $table->string('nome', 50);
-            $table->string('telefone', 20);
-            $table->string('email', 80);
-            $table->integer('motivo_contato');
-            $table->text('mensagem');
+            $table->timestamps();
         });
     }
 
@@ -30,7 +26,10 @@ return new class extends Migration
      * @return void
      */
     public function down()
-    {
-        Schema::dropIfExists('site_contatos');
+    {   
+        //remove um schema (tabela) caso essa tabela exista
+        Schema::dropIfExists('fornecedores');
+        //remove sem fazer um teste prévio
+        //Schema::drop('fornecedores');
     }
 };
